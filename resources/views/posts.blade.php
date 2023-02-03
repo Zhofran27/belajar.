8deb8f1234833e834275af082725c2e28f529bfd
Zhofran27 <Zhofran275@gmail.com>
2023-02-03 16:37:58 +0700

    Menambahkan contoh isi blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,5 +5,16 @@
 @section('container')
     
 
-    <h1>halaman Posts</h1>
+    @foreach ($posts as $post)
+    <article class="mb-5">
+        <h2>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }} </a>
+        </h2>
+        <h5>By: {{ $post["author"] }}</h5>
+        <p>{{ $post["body"] }}</p>
+    </article>
+    @endforeach
  @endsection
+
+
+ 
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Muhammad Zhofran",
         "email" => "zhofran275@gmail.com",
         "image" => "zee.jpg"
     ]);
 });
+
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul pertama",
+            "slug" => "judul-pertama",
+            "author" => "Muhammad Zhofran",
+            "body" =>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nesciunt perspiciatis voluptatibus corporis distinctio velit, natus sed molestias dolor molestiae beatae rerum, quaerat ullam explicabo impedit nemo commodi labore consequuntur laboriosam dignissimos fugiat? Tempora, deserunt! Eveniet corporis veritatis enim ut mollitia maxime rem voluptates facilis doloribus? In rerum delectus tenetur nulla totam mollitia ea aliquam reiciendis! Necessitatibus, dolore voluptas magni esse cupiditate aliquam quae soluta in quaerat aut officiis temporibus impedit iure nobis eius repellendus mollitia aspernatur voluptatibus. Ipsa, necessitatibus."
+        ],
+        [
+            "title" => "Judul kedua",
+            "slug" => "judul-kedua",
+            "author" => "Muhammad Zhofran",
+            "body" =>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nesciunt perspiciatis voluptatibus corporis distinctio velit, natus sed molestias dolor molestiae beatae rerum, quaerat ullam explicabo impedit nemo commodi labore consequuntur laboriosam dignissimos fugiat? Tempora, deserunt! Eveniet corporis veritatis enim ut mollitia maxime rem voluptates facilis doloribus? In rerum delectus tenetur nulla totam mollitia ea aliquam reiciendis! Necessitatibus, dolore voluptas magni esse cupiditate aliquam quae soluta in quaerat aut officiis temporibus impedit iure nobis eius repellendus mollitia aspernatur voluptatibus. Ipsa, necessitatibus."
+        ]
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul pertama",
+            "slug" => "judul-pertama",
+            "author" => "Muhammad Zhofran",
+            "body" =>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nesciunt perspiciatis voluptatibus corporis distinctio velit, natus sed molestias dolor molestiae beatae rerum, quaerat ullam explicabo impedit nemo commodi labore consequuntur laboriosam dignissimos fugiat? Tempora, deserunt! Eveniet corporis veritatis enim ut mollitia maxime rem voluptates facilis doloribus? In rerum delectus tenetur nulla totam mollitia ea aliquam reiciendis! Necessitatibus, dolore voluptas magni esse cupiditate aliquam quae soluta in quaerat aut officiis temporibus impedit iure nobis eius repellendus mollitia aspernatur voluptatibus. Ipsa, necessitatibus."
+        ],
+        [
+            "title" => "Judul kedua",
+            "slug" => "judul-kedua",
+            "author" => "Muhammad Zhofran",
+            "body" =>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nesciunt perspiciatis voluptatibus corporis distinctio velit, natus sed molestias dolor molestiae beatae rerum, quaerat ullam explicabo impedit nemo commodi labore consequuntur laboriosam dignissimos fugiat? Tempora, deserunt! Eveniet corporis veritatis enim ut mollitia maxime rem voluptates facilis doloribus? In rerum delectus tenetur nulla totam mollitia ea aliquam reiciendis! Necessitatibus, dolore voluptas magni esse cupiditate aliquam quae soluta in quaerat aut officiis temporibus impedit iure nobis eius repellendus mollitia aspernatur voluptatibus. Ipsa, necessitatibus."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post){
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
